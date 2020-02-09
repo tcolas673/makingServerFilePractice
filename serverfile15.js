c1ff424a5c145a09ba93dbd50e59e88612cdd12f
tcolas673 <tcolas673@gmail.com>
2020-02-09 15:35:33 -0500

    I am satisfied now

---
+++ serverfile15.js
@@ -0,0 +1,56 @@
+// all 3
+
+// http only and ran successfully
+// const http = require('http');
+// const PORT = 3000;
+// const requestHandler = (req, res) =>{
+//     res.end('Huge progress here');
+// }
+
+// const server = http.createServer(requestHandler);
+
+// server.listen(PORT, (err) =>{
+//     if(err) console.log('we got probs son');
+//     console.log('server is running!');
+// })
+
+// my http server with express
+// ran successfully
+// const http = require('http');
+// const express = require('express');
+
+// const PORT = 3000;
+
+// const requestHandler = (req, res) =>{
+//     res.end('Huge progress here');
+// }
+
+// const app = express();
+
+// app.use('/', requestHandler);
+
+// const server = http.createServer(app);
+
+// server.listen(PORT, (err) =>{
+//         if(err) console.log('we got probs son');
+//         console.log('server is running!');
+//     })
+
+
+// express only
+
+const express = require('express');
+
+const app = express();
+const requestHandler = (req, res) =>{
+    res.end('Did all 3 in less than 15 minutes');
+}
+
+app.use('/', requestHandler);
+
+app.listen(3000, (err)=>{
+    if(err) console.log('we got probs son!')
+    console.log('I\'ve improved');
+})
+
+

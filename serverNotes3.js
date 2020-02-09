be0300d2aea8902fd60c354a02087cfd41c49c5f
tcolas673 <tcolas673@gmail.com>
2020-02-09 15:16:53 -0500

    got the full picture

---
+++ serverNotes3.js
@@ -0,0 +1,12 @@
+// essentially express is allowing us to handle routing and doing other things
+// without us having to write a bunch of redundant crap
+
+// Express is a routing and middleware web framework that has minimal functionality of its own:
+// An Express application is essentially a series of middleware function calls.
+// reference serverfile14.js to see the nightmare
+
+// what is middleware?
+// Middleware is software that provides common services and capabilities to 
+// applications outside of what’s offered by the operating system. 
+// Data management, application services, messaging, authentication, 
+// and API management are all commonly handled by middleware

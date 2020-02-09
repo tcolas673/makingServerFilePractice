44494e47c39a5fa5ad1a428ff5fb63114dab14ab
tcolas673 <tcolas673@gmail.com>
2020-02-09 17:22:38 -0500

    looking at handler file logic

---
+++ serverNotes4.js
@@ -0,0 +1,8 @@
+// What exactly is the Express Router? It is a mini 
+// express application without all the bells and whistles 
+// of an express application, just the routing stuff.
+
+// so I could set up a file to handle routing for my server and use 
+// const UserRouter = require('express').Router()
+
+// https://scotch.io/tutorials/learn-to-use-the-new-router-in-expressjs-4
